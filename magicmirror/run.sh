095252329346bbe9d25ceffdf1e0b4322fee4bbc
iablon <64681499+iablon@users.noreply.github.com>
2023-09-26 16:13:42 +0000

    added ingress proxy

--- magicmirror/run.sh
+++ magicmirror/run.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/with-contenv bashio
 if [ ! -d "/share/MagicMirror" ]; then
+    bashio::log.info "copying default files to homeassistant into /share/Magicmirror..."
     mkdir -p /share/MagicMirror/config \
     && mkdir -p /share/MagicMirror/css \
     && mkdir -p /share/MagicMirror/modules \
@@ -8,13 +9,15 @@ if [ ! -d "/share/MagicMirror" ]; then
     && cp -a /home/MagicMirror/modules/. /share/MagicMirror/modules
 fi
 
+bashio::log.info "removing old files from Magicmirror..."
 rm -r /home/MagicMirror/config/*
 rm -r /home/MagicMirror/css/*
 rm -r /home/MagicMirror/modules/*
 
+bashio::log.info "copying new files to Magicmirror..."
 cp -a /share/MagicMirror/config/. /home/MagicMirror/config/
 cp -a /share/MagicMirror/css/. /home/MagicMirror/css/
 cp -a /share/MagicMirror/modules/. /home/MagicMirror/modules
 
-
+bashio::log.info "starting Magicmirror..."
 npm --prefix /home/MagicMirror run server
