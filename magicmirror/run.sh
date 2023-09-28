b2b7499d864d91f9fd058611e763ab5d43d0b63d
iablon <64681499+iablon@users.noreply.github.com>
2023-09-28 07:32:44 +0200

    Update run.sh

--- magicmirror/run.sh
+++ magicmirror/run.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/with-contenv bashio
 if [ ! -d "/share/MagicMirror" ]; then
-<<<<<<< HEAD
     bashio::log.info "copying default files to homeassistant into /share/Magicmirror..."
-=======
-    bashio::log.info "copying default config to homeassistant..."
->>>>>>> a822217e4c4bb2e663815389dd0456cbd2ddaad5
     mkdir -p /share/MagicMirror/config \
     && mkdir -p /share/MagicMirror/css \
     && mkdir -p /share/MagicMirror/modules \
@@ -21,7 +17,6 @@ fi
     cp -a /share/MagicMirror/css/. /home/MagicMirror/css/
     cp -a /share/MagicMirror/modules/. /home/MagicMirror/modules
 
-<<<<<<< HEAD
 bashio::log.info "removing old files from Magicmirror..."
 rm -r /home/MagicMirror/config/*
 rm -r /home/MagicMirror/css/*
@@ -34,7 +29,3 @@ cp -a /share/MagicMirror/modules/. /home/MagicMirror/modules
 
 bashio::log.info "starting Magicmirror..."
 npm --prefix /home/MagicMirror run server
-=======
-bashio::log.info "starting MagicMirror..."
-npm --prefix /home/MagicMirror run server
->>>>>>> a822217e4c4bb2e663815389dd0456cbd2ddaad5
